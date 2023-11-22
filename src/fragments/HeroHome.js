a625c0234b4c34ccf1a7229fa4b11a61c6fa5f90
joerhoney <6787748+joerhoney@users.noreply.github.com>
2023-11-22 13:56:58 -0800

    Fixing overflow issue.

--- src/fragments/HeroHome.js
+++ src/fragments/HeroHome.js
@@ -3,71 +3,73 @@ import * as clss from "../css/heroHome.module.css";
 
 const HeroHome = () => {
   return (
-    <div className={clss.heroWrapper}>
-      <div className={clss.heroHome}>
-        <h1 className={clss.joerhoney}>
-          <b>Joe</b> <b>Rhoney</b>
-        </h1>
-        <div className={clss.jBig}>
-          <p>
-            UI/&#8203;UX, re&shy;spon&shy;sive lay&shy;out,
-            hard&shy;ware-ac&shy;cel&shy;er&shy;at&shy;ed
-            graph&shy;ics/&#8203;an&shy;i&shy;ma&shy;tion,
-            ac&shy;ces&shy;si&shy;bili&shy;ty, search en&shy;gine
-            op&shy;ti&shy;miza&shy;tion, de&shy;bug&shy;ging,
-            de&shy;vice/&#8203;brows&shy;er
-            com&shy;pat&shy;i&shy;bil&shy;i&shy;ty, adap&shy;tive out&shy;put,
-            CSS3, HTML5, Ja&shy;va&shy;Script, ES6, React, JSON, jQuery, SVG,
-            PHP, CLIs (Bash, Z shell, Git, Git&shy;Hub), SQL, All ma&shy;jor
-            brows&shy;ers and their in&shy;spec&shy;tion tools, VS&shy;Code (and
-            other IDEs), Git&shy;Hub Desk&shy;top, Cy&shy;ber&shy;Duck,
-            File&shy;Zilla, Adobe Cre&shy;ative Suite (Adobe
-            Pho&shy;to&shy;shop, Adobe Il&shy;lus&shy;tra&shy;tor, Adobe XD,
-            Adobe Pre&shy;miere, Adobe After&shy;Ef&shy;fects),
-            Af&shy;fin&shy;i&shy;ty Suite (Af&shy;fin&shy;i&shy;ty Pho&shy;to 2,
-            Af&shy;fin&shy;i&shy;ty De&shy;sign&shy;er 2,
-            Af&shy;fin&shy;i&shy;ty Pub&shy;lish&shy;er), GIMP, Ink&shy;Scape,
-            At&shy;lass&shy;ian, Figma, Teal&shy;ium, Work&shy;Place, Slack,
-            Zoom, Site&shy;Im&shy;prove, Pay&shy;Pal, WC3 Mark&shy;up
-            Val&shy;i&shy;da&shy;tion, Google Search&shy;Con&shy;sole,
-            Page&shy;Speed In&shy;sights, Mo&shy;bile-Friend&shy;ly Test,
-            Reg&shy;Exr, Acu&shy;net&shy;ix, Brows&shy;er&shy;Stack,
-            Se&shy;mant&shy;ics, nam&shy;ing con&shy;ven&shy;tion and code
-            for&shy;mat ad&shy;her&shy;ence,
-            cross-brows&shy;er/&#8203;de&shy;vice
-            com&shy;pat&shy;i&shy;bil&shy;i&shy;ty, W3C com&shy;pli&shy;ance,
-            ac&shy;cess&shy;i&shy;bil&shy;i&shy;ty, SEO page ba&shy;sics,
-            con&shy;tin&shy;ued educa&shy;tion, Custom com&shy;pon&shy;ent
-            librar&shy;ies, Hack/&#8203;XHP, React Na&shy;tive, Python, Perl,
-            .NET, ASP, CSS pro&shy;cess&shy;ors (SCSS, etc.), Boot&shy;strap,
-            scrum pro&shy;cess, us&shy;ing macOS, Windows, Linux OSs. Do you
-            like this typographic layout? Learn more about it on my{" "}
-            <a
-              href="https://codepen.io/joerhoney/pen/bGOypwq"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Codepen
-            </a>
-          </p>
-        </div>
-        <div className={clss.jDots}>
-          <p>
-            Il&shy;lus&shy;tra&shy;tion, an&shy;i&shy;ma&shy;tion, video
-            ed&shy;i&shy;ting, pho&shy;tog&shy;ra&shy;phy, lay&shy;out,
-            col&shy;or de&shy;sign, ty&shy;pog&shy;ra&shy;phy,
-            char&shy;ac&shy;ter/&#8203;crea&shy;ture de&shy;sign,
-            con&shy;cep&shy;tu&shy;al art, back&shy;ground art, life
-            draw&shy;ing, car&shy;i&shy;ca&shy;tures Dig&shy;i&shy;tal art
-            tab&shy;let, ink, pen&shy;cil/&#8203;graphite, char&shy;coal,
-            col&shy;ored pen&shy;cil, pas&shy;tel, paint,
-            wa&shy;ter&shy;col&shy;or, Pan&shy;tone Col&shy;or Chart, Adobe
-            Il&shy;lus&shy;tra&shy;tor, Adobe Pho&shy;to&shy;shop,
-            Af&shy;fin&shy;i&shy;ty Pho&shy;to 2, Af&shy;fin&shy;i&shy;ty
-            De&shy;sign&shy;er 2. This lay&shy;out uses shape-out&shy;side to
-            pro&shy;duce a shape-in&shy;side ef&shy;fect (since
-            shape-in&shy;side still isn't sup&shy;por&shy;ted yet).
-          </p>
+    <div className={clss.heroClip}>
+      <div className={clss.heroWrapper}>
+        <div className={clss.heroHome}>
+          <h1 className={clss.joerhoney}>
+            <b>Joe</b> <b>Rhoney</b>
+          </h1>
+          <div className={clss.jBig}>
+            <p>
+              UI/&#8203;UX, re&shy;spon&shy;sive lay&shy;out,
+              hard&shy;ware-ac&shy;cel&shy;er&shy;at&shy;ed
+              graph&shy;ics/&#8203;an&shy;i&shy;ma&shy;tion,
+              ac&shy;ces&shy;si&shy;bili&shy;ty, search en&shy;gine
+              op&shy;ti&shy;miza&shy;tion, de&shy;bug&shy;ging,
+              de&shy;vice/&#8203;brows&shy;er
+              com&shy;pat&shy;i&shy;bil&shy;i&shy;ty, adap&shy;tive out&shy;put,
+              CSS3, HTML5, Ja&shy;va&shy;Script, ES6, React, JSON, jQuery, SVG,
+              PHP, CLIs (Bash, Z shell, Git, Git&shy;Hub), SQL, All ma&shy;jor
+              brows&shy;ers and their in&shy;spec&shy;tion tools, VS&shy;Code
+              (and other IDEs), Git&shy;Hub Desk&shy;top, Cy&shy;ber&shy;Duck,
+              File&shy;Zilla, Adobe Cre&shy;ative Suite (Adobe
+              Pho&shy;to&shy;shop, Adobe Il&shy;lus&shy;tra&shy;tor, Adobe XD,
+              Adobe Pre&shy;miere, Adobe After&shy;Ef&shy;fects),
+              Af&shy;fin&shy;i&shy;ty Suite (Af&shy;fin&shy;i&shy;ty Pho&shy;to
+              2, Af&shy;fin&shy;i&shy;ty De&shy;sign&shy;er 2,
+              Af&shy;fin&shy;i&shy;ty Pub&shy;lish&shy;er), GIMP, Ink&shy;Scape,
+              At&shy;lass&shy;ian, Figma, Teal&shy;ium, Work&shy;Place, Slack,
+              Zoom, Site&shy;Im&shy;prove, Pay&shy;Pal, WC3 Mark&shy;up
+              Val&shy;i&shy;da&shy;tion, Google Search&shy;Con&shy;sole,
+              Page&shy;Speed In&shy;sights, Mo&shy;bile-Friend&shy;ly Test,
+              Reg&shy;Exr, Acu&shy;net&shy;ix, Brows&shy;er&shy;Stack,
+              Se&shy;mant&shy;ics, nam&shy;ing con&shy;ven&shy;tion and code
+              for&shy;mat ad&shy;her&shy;ence,
+              cross-brows&shy;er/&#8203;de&shy;vice
+              com&shy;pat&shy;i&shy;bil&shy;i&shy;ty, W3C com&shy;pli&shy;ance,
+              ac&shy;cess&shy;i&shy;bil&shy;i&shy;ty, SEO page ba&shy;sics,
+              con&shy;tin&shy;ued educa&shy;tion, Custom com&shy;pon&shy;ent
+              librar&shy;ies, Hack/&#8203;XHP, React Na&shy;tive, Python, Perl,
+              .NET, ASP, CSS pro&shy;cess&shy;ors (SCSS, etc.), Boot&shy;strap,
+              scrum pro&shy;cess, us&shy;ing macOS, Windows, Linux OSs. Do you
+              like this typographic layout? Learn more about it on my{" "}
+              <a
+                href="https://codepen.io/joerhoney/pen/bGOypwq"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Codepen
+              </a>
+            </p>
+          </div>
+          <div className={clss.jDots}>
+            <p>
+              Il&shy;lus&shy;tra&shy;tion, an&shy;i&shy;ma&shy;tion, video
+              ed&shy;i&shy;ting, pho&shy;tog&shy;ra&shy;phy, lay&shy;out,
+              col&shy;or de&shy;sign, ty&shy;pog&shy;ra&shy;phy,
+              char&shy;ac&shy;ter/&#8203;crea&shy;ture de&shy;sign,
+              con&shy;cep&shy;tu&shy;al art, back&shy;ground art, life
+              draw&shy;ing, car&shy;i&shy;ca&shy;tures Dig&shy;i&shy;tal art
+              tab&shy;let, ink, pen&shy;cil/&#8203;graphite, char&shy;coal,
+              col&shy;ored pen&shy;cil, pas&shy;tel, paint,
+              wa&shy;ter&shy;col&shy;or, Pan&shy;tone Col&shy;or Chart, Adobe
+              Il&shy;lus&shy;tra&shy;tor, Adobe Pho&shy;to&shy;shop,
+              Af&shy;fin&shy;i&shy;ty Pho&shy;to 2, Af&shy;fin&shy;i&shy;ty
+              De&shy;sign&shy;er 2. This lay&shy;out uses shape-out&shy;side to
+              pro&shy;duce a shape-in&shy;side ef&shy;fect (since
+              shape-in&shy;side still isn't sup&shy;por&shy;ted yet).
+            </p>
+          </div>
         </div>
       </div>
     </div>
