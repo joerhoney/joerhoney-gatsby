ebbf4ccd8ff981accbbc48b117f64012eae149dc
joerhoney <6787748+joerhoney@users.noreply.github.com>
2023-12-02 16:34:01 -0800

    Updated manifest background_color

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,11 +30,11 @@ module.exports = {
         name: ":joe rhoney",
         short_name: ":joe rhoney",
         start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        background_color: "#121f26",
+        theme_color: "#121f26",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
+        display: "minimal-ui",
         icon: "src/images/icon.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
