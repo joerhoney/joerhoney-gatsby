5311e60ecd6c4c44e6acd4f0d23697f86d9906cc
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-05-10 10:33:35 -0700

    Found and fixed error in dev mode.

--- src/layouts/Scheme.js
+++ src/layouts/Scheme.js
@@ -3,60 +3,69 @@ import { Script } from "gatsby";
 // CSS
 import "@css/scheme.css";
 
-const Scheme = () => {
-  return (
-    <>
-      <fieldset id="color-scheme">
-        <label
-          className="light-scheme-icon"
-          htmlFor="light-scheme"
-          title="Light color scheme"
-        >
-          <input
-            name="color-scheme"
-            id="light-scheme"
-            type="radio"
-            value="light"
-            onClick={() => {
-              localStorage.setItem("colorScheme", "light");
-              console.log(localStorage.getItem("colorScheme"));
-            }}
-          />
-        </label>
-        <label
-          className="auto-scheme-icon"
-          htmlFor="auto-scheme"
-          title="Your default color scheme"
-        >
-          <input
-            name="color-scheme"
-            id="auto-scheme"
-            type="radio"
-            value="auto"
-            onClick={() => {
-              localStorage.setItem("colorScheme", "auto");
-              console.log(localStorage.getItem("colorScheme"));
-            }}
-          />
-        </label>
-        <label
-          className="dark-scheme-icon"
-          htmlFor="dark-scheme"
-          title="Dark color scheme"
-        >
-          <input
-            name="color-scheme"
-            id="dark-scheme"
-            type="radio"
-            value="dark"
-            onClick={() => {
-              localStorage.setItem("colorScheme", "dark");
-              console.log(localStorage.getItem("colorScheme"));
-            }}
-          />
-        </label>
-      </fieldset>
-      <script id="second-unique-id">{`
+class Scheme extends React.Component {
+  componentDidMount() {
+    console.log(localStorage.getItem("colorScheme"));
+    document
+      .getElementById("color-scheme")
+      .querySelector(
+        `input[value=${localStorage.getItem("colorScheme") ?? "auto"}]`
+      ).checked = true;
+  }
+  render() {
+    return (
+      <>
+        <fieldset id="color-scheme">
+          <label
+            className="light-scheme-icon"
+            htmlFor="light-scheme"
+            title="Light color scheme"
+          >
+            <input
+              name="color-scheme"
+              id="light-scheme"
+              type="radio"
+              value="light"
+              onClick={() => {
+                localStorage.setItem("colorScheme", "light");
+                console.log(localStorage.getItem("colorScheme"));
+              }}
+            />
+          </label>
+          <label
+            className="auto-scheme-icon"
+            htmlFor="auto-scheme"
+            title="Your default color scheme"
+          >
+            <input
+              name="color-scheme"
+              id="auto-scheme"
+              type="radio"
+              value="auto"
+              onClick={() => {
+                localStorage.setItem("colorScheme", "auto");
+                console.log(localStorage.getItem("colorScheme"));
+              }}
+            />
+          </label>
+          <label
+            className="dark-scheme-icon"
+            htmlFor="dark-scheme"
+            title="Dark color scheme"
+          >
+            <input
+              name="color-scheme"
+              id="dark-scheme"
+              type="radio"
+              value="dark"
+              onClick={() => {
+                localStorage.setItem("colorScheme", "dark");
+                console.log(localStorage.getItem("colorScheme"));
+              }}
+            />
+          </label>
+        </fieldset>
+        {/* <script>{`
 window.onload = () => {
   console.log(localStorage.getItem("colorScheme"));
   document
@@ -64,9 +73,10 @@ window.onload = () => {
     .querySelector(
       \`input[value=\${localStorage.getItem("colorScheme") ?? "auto"}]\`
     ).checked = true;
-};`}</script>
-    </>
-  );
-};
+};`}</script> */}
+      </>
+    );
+  }
+}
 
 export default Scheme;
