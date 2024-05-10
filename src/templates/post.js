4b8c28c185d4b66b15a7f5f8d328f3c8bb1a25f7
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-05-09 20:56:12 -0700

    Trying a template literal instead of concatination.

--- src/templates/post.js
+++ src/templates/post.js
@@ -41,7 +41,7 @@ export const query = graphql`
 const Post = (props) => {
   console.log(props.data.contentfulPost);
   const { date, title } = props.data.contentfulPost;
-  const featurl = "https:" + props.data.contentfulPost.featuredImage.file.url;
+  const featurl = `https:${props.data.contentfulPost.featuredImage.file.url}`;
   const featdesc = props.data.contentfulPost.featuredImage.description;
   console.log(featurl);
   const html = JSON.parse(props.data.contentfulPost.body.raw);
