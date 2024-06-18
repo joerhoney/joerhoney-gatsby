ba799f650dcca1af2d4fa7d2eb58548183d3bafc
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-06-18 14:13:40 -0700

    I think I need method=POST. Trying that.

--- src/components/FormNetlify.js
+++ src/components/FormNetlify.js
@@ -6,7 +6,7 @@ import "@css/form.css";
 const Form = (props) => {
   return (
     <div className="contact">
-      <form name="contact" className={props.className} netlify>
+      <form name="contact" className={props.className} method="POST" netlify>
         <input type="text" name="name" placeholder="Full Name" required />
         <input type="email" name="email" placeholder="Email Address" required />
         <input
