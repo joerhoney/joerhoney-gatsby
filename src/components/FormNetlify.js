0781761544679b49a19996016b120682a3563a14
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-06-18 13:59:03 -0700

    Trying out Netlify Form Detection.

---
+++ src/components/FormNetlify.js
@@ -0,0 +1,41 @@
+import React from "react";
+// CSS
+import "@css/buttons.css";
+import "@css/form.css";
+
+const Form = (props) => {
+  return (
+    <div className="contact">
+      <form name="contact" className={props.className} netlify>
+        <input type="text" name="name" placeholder="Full Name" required />
+        <input type="email" name="email" placeholder="Email Address" required />
+        <input
+          type="text"
+          name="_subject"
+          placeholder="Subject Line"
+          required
+        />
+        <input
+          type="url"
+          name="website"
+          placeholder="Website (if you have one)"
+        />
+        <textarea
+          placeholder="Your Message"
+          name="message"
+          rows="10"
+          required
+        ></textarea>
+        <button type="submit">Send</button>
+        <input type="hidden" name="_next" value="/thanks-contact" />
+        <input
+          type="hidden"
+          name="_url"
+          value="https://joerhoney.com/#contact"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
