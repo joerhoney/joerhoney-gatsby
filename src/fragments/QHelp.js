18372c0db13702638d61dd95b7d66af5001cf099
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-06-21 09:51:16 -0700

    Changed name QProblems.js to QHelp.js.

--- src/fragments/QHelp.js
+++ src/fragments/QHelp.js
@@ -6,29 +6,27 @@ import { Wizard, Q, A, Step, End } from "../components/Wizard";
 import ButtonContact from "@fragments/ButtonContact";
 import ButtonWebsitesPackages from "@fragments/ButtonWebsitesPackages";
 import ButtonWebMaintenancePlans from "@fragments/ButtonWebMaintenancePlans";
-import HowFindWebsite from "@fragments/QProblems_HowFind";
+import HowFindWebsite from "@fragments/QHelp_HowFind";
 
-function QProblems() {
+function QHelp() {
   return (
     <Wizard start="qa_Help" name="problems">
       <Step id="qa_Help">
-        <Q>What web problems/challenges do you need help with?</Q>
+        <Q>What do you need help with?</Q>
+        <A a="A new website." goto="qa_NewWebsite" />
         <A a="An existing website." goto="qa_XWebsite">
           <Step id="qa_XWebsite">
-            <Q>Tell me about your website.</Q>
-            <A
-              a="It seems to be missing or broken."
-              goto="qa_WebsiteBroken"
-            ></A>
+            <Q>Tell me about your website problem.</Q>
+            <A a="It seems to be missing or broken." goto="qa_WebsiteBroken" />
             <A a="It's not working right." goto="qa_WebsiteBroken"></A>
             <A
               a="It doesn't get enough/any traffic."
               goto="qa_WebsiteTraffic"
-            ></A>
-            <A a="It doesn't retain visitors." goto="qa_WebsiteRetention"></A>
-            <A a="It's slow/laggy." goto="qa_WebsiteSlow"></A>
-            <A a="It's unprofessional/ugly." goto="qa_WebsiteUgly"></A>
-            <A a="The content is out-dated." goto="qa_WebsiteOutdated"></A>
+            />
+            <A a="It doesn't retain visitors." goto="qa_WebsiteRetention" />
+            <A a="It's slow/laggy." goto="qa_WebsiteSlow" />
+            <A a="It's unprofessional/ugly." goto="qa_WebsiteUgly" />
+            <A a="The content is out-dated." goto="qa_WebsiteOutdated" />
             <A a="I don't know / I lost track of it." goto="qa_WebsiteIDK">
               <Step id="qa_WebsiteIDK">
                 <Q>What data are you missing?</Q>
@@ -73,42 +71,66 @@ function QProblems() {
             </A>
           </Step>
         </A>
-        <A a="I need a new website." goto="qa_NewWebsite">
-          <End id="qa_NewWebsite">
-            <p>Great! Please see my website packages:</p>
-            <ButtonWebsitesPackages />
-          </End>
+        <A a="Art/design work." goto="qa_ArtDesign">
+          <Step id="qa_ArtDesign">
+            <Q>What kind of creative work do you need?</Q>
+            <A a="Web, print or other design work." goto="qa_Design" />
+            <A
+              a="Illustration, animation or other creative services."
+              goto="qa_Art"
+            />
+          </Step>
         </A>
-        <A a="Website maintenance." goto="qa_Maintenance">
-          <End id="qa_Maintenance">
-            <p>No problem! Please see my web maintenance plans:</p>
-            <ButtonWebMaintenancePlans />
-          </End>
-        </A>
-        <A a="Web design." goto="qa_Design">
-          <End id="qa_Design">
-            <p>
-              Awesome! Let's get in touch so we can work together on your design
-              needs:
-            </p>
-            <ButtonContact />
-          </End>
-        </A>
-        <A a="Web presence." goto="qa_Presence">
-          <End id="qa_Presence">
-            <p>
-              Got it! Please contact me so I can learn more about your business
-              and help you determine what we need to do to build your web
-              presence:
-            </p>
-            <ButtonContact />
-          </End>
+        <A
+          a="Other web services not specific to a website."
+          goto="qa_WebNonSite"
+        >
+          <Step id="qa_WebNonSite">
+            <Q>What kind of "non-website" web services do you need?</Q>
+            <A
+              a="Local online marketing (Google Maps, Yelp!, Facebook, etc.)."
+              goto="qa_Presence"
+            />
+            <A a="Social media branding." goto="qa_Presence"></A>
+            <A a="Social media content." goto="qa_Presence"></A>
+            <A
+              a="Whatever branding/marketing my business could gain more web traffic from."
+              goto="qa_Presence"
+            />
+            <A a="Something else." goto="qa_Presence"></A>
+          </Step>
         </A>
         <A a="What services to you offer?" goto="#services" />
         <A a="How can I contact you?" goto="#contact" />
       </Step>
+      <End id="qa_NewWebsite">
+        <p>Great! Please see my website packages:</p>
+        <ButtonWebsitesPackages />
+      </End>
+      <End id="qa_Maintenance">
+        <p>No problem! Please see my web maintenance plans:</p>
+        <ButtonWebMaintenancePlans />
+      </End>
+      <End id="qa_Design">
+        <p>
+          Awesome! Let's get in touch so we can work together on your design
+          needs:
+        </p>
+        <ButtonContact />
+      </End>
+      <End id="qa_Art">
+        <p>Nice! Please contact me so we can discuss your creative project:</p>
+        <ButtonContact />
+      </End>
+      <End id="qa_Presence">
+        <p>
+          Got it! Please contact me so I can learn more about your business and
+          help you determine what we need to do to build your web presence:
+        </p>
+        <ButtonContact />
+      </End>
     </Wizard>
   );
 }
 
-export default QProblems;
+export default QHelp;
