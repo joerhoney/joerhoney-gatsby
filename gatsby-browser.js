598ff5da5c02d027d2cdb60e24b2cae62e37c674
joerhoney <6787748+joerhoney@users.noreply.github.com>
2024-08-28 19:18:29 -0700

    Figuring out how to theme Prism.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,22 @@
-import "/src/css/prism.css";
+// import "/src/css/prism.css";
+
+// import "prismjs/plugins/command-line/prism-command-line.css";
+
+require("/src/css/prism.css");
+
+// prismjs/themes/prism-coy.css
+// prismjs/themes/prism-coy.min.css
+// prismjs/themes/prism-dark.css
+// prismjs/themes/prism-dark.min.css
+// prismjs/themes/prism-funky.css
+// prismjs/themes/prism-funky.min.css
+// prismjs/themes/prism-okaidia.css
+// prismjs/themes/prism-okaidia.min.css
+// prismjs/themes/prism-solarizedlight.css
+// prismjs/themes/prism-solarizedlight.min.css
+// prismjs/themes/prism-tomorrow.css
+// prismjs/themes/prism-tomorrow.min.css
+// prismjs/themes/prism-twilight.css
+// prismjs/themes/prism-twilight.min.css
+// prismjs/themes/prism.css
+// prismjs/themes/prism.min.css
