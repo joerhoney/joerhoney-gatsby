78539122dca4543ec6fdbe0a4168afcd94f1d6b7
Joe Rhoney <6787748+joerhoney@users.noreply.github.com>
2024-09-05 11:45:26 -0700

    Added parent field to blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -33,6 +33,7 @@ const Blog = () => {
               draft
             }
             fields {
+              parent
               slug
             }
           }
@@ -55,6 +56,8 @@ const Blog = () => {
       <main className="blog">
         <h1 className="h1 square">Blog</h1>
         {posts.map((post) => {
+          console.log("parent: " + post.node.fields.parent);
+          console.log("slug: " + post.node.fields.slug);
           const { description, featimg, featalt, title } =
             post.node.frontmatter;
           const published = dateFormat(post.node.frontmatter.published);
